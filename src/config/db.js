85f539be9d100a82bc3c6db2dba9888aa9f59e81
chinnasamy <chinnasamyulaganathan31@gmail.com>
2026-05-08 15:37:27 +0530

    render postgres connected

--- src/config/db.js
+++ src/config/db.js
@@ -2,5 +2,5 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString="postgresql://auction:pE1Tv9mTG7Ty5EVoGiLijWtMuvWGk9wO@dpg-d7ur4hbrjlhs7397ecjg-a/auction_db_t9wb"
+  connectionString="postgresql://postgres:[YOUR-PASSWORD]@db.xzfcplbcanartidezjgp.supabase.co:5432/postgres"
 });
