12e474e03951f1629f6fc205012f8f50e64a637d
chinnasamy <chinnasamyulaganathan31@gmail.com>
2026-05-08 16:14:03 +0530

    fixed supabase connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,6 +1,10 @@
 import pkg from "pg";
+
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString="postgresql://postgres:[Auction_!@#$AT]@db.xzfcplbcanartidezjgp.supabase.co:5432/postgres"
+  connectionString: "postgresql://postgres:Auction_!%40%23%24AT@db.xzfcplbcanartidezjgp.supabase.co:5432/postgres",
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
