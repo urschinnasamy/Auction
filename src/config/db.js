065da1f01593c7a2f9170b1cee8d93fe12c2f251
chinnasamy <chinnasamyulaganathan31@gmail.com>
2026-05-08 16:26:40 +0530

    fixed supabase pooler connection

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,9 @@ import pkg from "pg";
 const { Pool } = pkg;
 
 export const pool = new Pool({
-  connectionString: "postgresql://postgres:Auction_!%40%23%24AT@db.xzfcplbcanartidezjgp.supabase.co:5432/postgres",
+  connectionString:
+    "postgresql://postgres.xzfcplbcanartidezjgp:Auction_!%40%23%24AT@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres",
+
   ssl: {
     rejectUnauthorized: false,
   },
